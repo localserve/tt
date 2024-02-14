c588da629ebe3b2820416117d751d0e67f7e9d06
Anubhav Saini <iamanubhavsaini+git@gmail.com>
2024-02-14 18:08:19 +0530

    gofmt, and use Enter to go back to testing

--- src/typer.go
+++ src/typer.go
@@ -1,415 +1,415 @@
 package main
 
 import (
-    "fmt"
-    "io"
-    "io/ioutil"
-    "os"
-    "runtime"
-    "strconv"
-    "time"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"runtime"
+	"strconv"
+	"time"
 
-    "github.com/gdamore/tcell"
+	"github.com/gdamore/tcell"
 )
 
 const (
-    TyperComplete = iota
-    TyperSigInt
-    TyperEscape
-    TyperPrevious
-    TyperNext
-    TyperResize
+	TyperComplete = iota
+	TyperSigInt
+	TyperEscape
+	TyperPrevious
+	TyperNext
+	TyperResize
 )
 
 type segment struct {
-    Text        string `json:"text"`
-    Attribution string `json:"attribution"`
+	Text        string `json:"text"`
+	Attribution string `json:"attribution"`
 }
 
 type mistake struct {
-    Word  string `json:"word"`
-    Typed string `json:"typed"`
+	Word  string `json:"word"`
+	Typed string `json:"typed"`
 }
 
 type typer struct {
-    Scr              tcell.Screen
-    OnStart          func()
-    SkipWord         bool
-    ShowWpm          bool
-    DisableBackspace bool
-    BlockCursor      bool
-    tty              io.Writer
+	Scr              tcell.Screen
+	OnStart          func()
+	SkipWord         bool
+	ShowWpm          bool
+	DisableBackspace bool
+	BlockCursor      bool
+	tty              io.Writer
 
-    currentWordStyle    tcell.Style
-    nextWordStyle       tcell.Style
-    incorrectSpaceStyle tcell.Style
-    incorrectStyle      tcell.Style
-    correctStyle        tcell.Style
-    defaultStyle        tcell.Style
+	currentWordStyle    tcell.Style
+	nextWordStyle       tcell.Style
+	incorrectSpaceStyle tcell.Style
+	incorrectStyle      tcell.Style
+	correctStyle        tcell.Style
+	defaultStyle        tcell.Style
 }
 
 func NewTyper(scr tcell.Screen, emboldenTypedText bool, fgcol, bgcol, hicol, hicol2, hicol3, errcol tcell.Color) *typer {
-    var tty io.Writer
-    def := tcell.StyleDefault.
-        Foreground(fgcol).
-        Background(bgcol)
+	var tty io.Writer
+	def := tcell.StyleDefault.
+		Foreground(fgcol).
+		Background(bgcol)
 
-    tty, err := os.OpenFile("/dev/tty", os.O_WRONLY, 0)
-    //Will fail on windows, but tt is still mostly usable via tcell
-    if err != nil {
-        tty = ioutil.Discard
-    }
+	tty, err := os.OpenFile("/dev/tty", os.O_WRONLY, 0)
+	//Will fail on windows, but tt is still mostly usable via tcell
+	if err != nil {
+		tty = ioutil.Discard
+	}
 
-    correctStyle := def.Foreground(hicol)
-    if emboldenTypedText {
-        correctStyle = correctStyle.Bold(true)
-    }
+	correctStyle := def.Foreground(hicol)
+	if emboldenTypedText {
+		correctStyle = correctStyle.Bold(true)
+	}
 
-    return &typer{
-        Scr:      scr,
-        SkipWord: true,
-        tty:      tty,
+	return &typer{
+		Scr:      scr,
+		SkipWord: true,
+		tty:      tty,
 
-        defaultStyle:        def,
-        correctStyle:        correctStyle,
-        currentWordStyle:    def.Foreground(hicol2),
-        nextWordStyle:       def.Foreground(hicol3),
-        incorrectStyle:      def.Foreground(errcol),
-        incorrectSpaceStyle: def.Background(errcol),
-    }
+		defaultStyle:        def,
+		correctStyle:        correctStyle,
+		currentWordStyle:    def.Foreground(hicol2),
+		nextWordStyle:       def.Foreground(hicol3),
+		incorrectStyle:      def.Foreground(errcol),
+		incorrectSpaceStyle: def.Background(errcol),
+	}
 }
 
 func (t *typer) Start(text []segment, timeout time.Duration) (nerrs, ncorrect int, duration time.Duration, rc int, mistakes []mistake) {
-    timeLeft := timeout
+	timeLeft := timeout
 
-    for i, s := range text {
-        startImmediately := true
-        var d time.Duration
-        var e, c int
-        var m []mistake
+	for i, s := range text {
+		startImmediately := true
+		var d time.Duration
+		var e, c int
+		var m []mistake
 
-        if i == 0 {
-            startImmediately = false
-        }
+		if i == 0 {
+			startImmediately = false
+		}
 
-        e, c, rc, d, m = t.start(s.Text, timeLeft, startImmediately, s.Attribution)
+		e, c, rc, d, m = t.start(s.Text, timeLeft, startImmediately, s.Attribution)
 
-        nerrs += e
-        ncorrect += c
-        duration += d
-        mistakes = append(mistakes, m...)
+		nerrs += e
+		ncorrect += c
+		duration += d
+		mistakes = append(mistakes, m...)
 
-        if timeout != -1 {
-            timeLeft -= d
-            if timeLeft <= 0 {
-                return
-            }
-        }
+		if timeout != -1 {
+			timeLeft -= d
+			if timeLeft <= 0 {
+				return
+			}
+		}
 
-        if rc != TyperComplete {
-            return
-        }
-    }
+		if rc != TyperComplete {
+			return
+		}
+	}
 
-    return
+	return
 }
 
 func extractMistypedWords(text []rune, typed []rune) (mistakes []mistake) {
-    var w []rune
-    var t []rune
-    f := false
+	var w []rune
+	var t []rune
+	f := false
 
-    for i := range text {
-        if text[i] == ' ' {
-            if f {
-                mistakes = append(mistakes, mistake{string(w), string(t)})
-            }
+	for i := range text {
+		if text[i] == ' ' {
+			if f {
+				mistakes = append(mistakes, mistake{string(w), string(t)})
+			}
 
-            w = w[:0]
-            t = t[:0]
-            f = false
-            continue
-        }
+			w = w[:0]
+			t = t[:0]
+			f = false
+			continue
+		}
 
-        if text[i] != typed[i] {
-            f = true
-        }
+		if text[i] != typed[i] {
+			f = true
+		}
 
-        if text[i] == 0 {
-            w = append(w, '_')
-        } else {
-            w = append(w, text[i])
-        }
+		if text[i] == 0 {
+			w = append(w, '_')
+		} else {
+			w = append(w, text[i])
+		}
 
-        if typed[i] == 0 {
-            t = append(t, '_')
-        } else {
-            t = append(t, typed[i])
-        }
-    }
+		if typed[i] == 0 {
+			t = append(t, '_')
+		} else {
+			t = append(t, typed[i])
+		}
+	}
 
-    if f {
-        mistakes = append(mistakes, mistake{string(w), string(t)})
-    }
+	if f {
+		mistakes = append(mistakes, mistake{string(w), string(t)})
+	}
 
-    return
+	return
 }
 
 func (t *typer) start(s string, timeLimit time.Duration, startImmediately bool, attribution string) (nerrs int, ncorrect int, rc int, duration time.Duration, mistakes []mistake) {
-    var startTime time.Time
-    text := []rune(s)
-    typed := make([]rune, len(text))
+	var startTime time.Time
+	text := []rune(s)
+	typed := make([]rune, len(text))
 
-    sw, sh := scr.Size()
-    nc, nr := calcStringDimensions(s)
-    x := (sw - nc) / 2
-    y := (sh - nr) / 2
+	sw, sh := scr.Size()
+	nc, nr := calcStringDimensions(s)
+	x := (sw - nc) / 2
+	y := (sh - nr) / 2
 
-    if !t.BlockCursor {
-        t.tty.Write([]byte("\033[5 q"))
+	if !t.BlockCursor {
+		t.tty.Write([]byte("\033[5 q"))
 
-        //Assumes original cursor shape was a block (the one true cursor shape), there doesn't appear to be a
-        //good way to save/restore the shape if the user has changed it from the otcs.
-        defer t.tty.Write([]byte("\033[2 q"))
-    }
+		//Assumes original cursor shape was a block (the one true cursor shape), there doesn't appear to be a
+		//good way to save/restore the shape if the user has changed it from the otcs.
+		defer t.tty.Write([]byte("\033[2 q"))
+	}
 
-    t.Scr.SetStyle(t.defaultStyle)
-    idx := 0
+	t.Scr.SetStyle(t.defaultStyle)
+	idx := 0
 
-    calcStats := func() {
-        nerrs = 0
-        ncorrect = 0
+	calcStats := func() {
+		nerrs = 0
+		ncorrect = 0
 
-        mistakes = extractMistypedWords(text[:idx], typed[:idx])
+		mistakes = extractMistypedWords(text[:idx], typed[:idx])
 
-        for i := 0; i < idx; i++ {
-            if text[i] != '\n' {
-                if text[i] != typed[i] {
-                    nerrs++
-                } else {
-                    ncorrect++
-                }
-            }
-        }
+		for i := 0; i < idx; i++ {
+			if text[i] != '\n' {
+				if text[i] != typed[i] {
+					nerrs++
+				} else {
+					ncorrect++
+				}
+			}
+		}
 
-        rc = TyperComplete
-        duration = time.Now().Sub(startTime)
-    }
+		rc = TyperComplete
+		duration = time.Now().Sub(startTime)
+	}
 
-    redraw := func() {
-        cx := x
-        cy := y
-        inword := -1
+	redraw := func() {
+		cx := x
+		cy := y
+		inword := -1
 
-        for i := range text {
-            style := t.defaultStyle
+		for i := range text {
+			style := t.defaultStyle
 
-            if text[i] == '\n' {
-                cy++
-                cx = x
-                if inword != -1 {
-                    inword++
-                }
-                continue
-            }
+			if text[i] == '\n' {
+				cy++
+				cx = x
+				if inword != -1 {
+					inword++
+				}
+				continue
+			}
 
-            if i == idx {
-                scr.ShowCursor(cx, cy)
-                inword = 0
-            }
+			if i == idx {
+				scr.ShowCursor(cx, cy)
+				inword = 0
+			}
 
-            if i >= idx {
-                if text[i] == ' ' {
-                    inword++
-                } else if inword == 0 {
-                    style = t.currentWordStyle
-                } else if inword == 1 {
-                    style = t.nextWordStyle
-                } else {
-                    style = t.defaultStyle
-                }
-            } else if text[i] != typed[i] {
-                if text[i] == ' ' {
-                    style = t.incorrectSpaceStyle
-                } else {
-                    style = t.incorrectStyle
-                }
-            } else {
-                style = t.correctStyle
-            }
+			if i >= idx {
+				if text[i] == ' ' {
+					inword++
+				} else if inword == 0 {
+					style = t.currentWordStyle
+				} else if inword == 1 {
+					style = t.nextWordStyle
+				} else {
+					style = t.defaultStyle
+				}
+			} else if text[i] != typed[i] {
+				if text[i] == ' ' {
+					style = t.incorrectSpaceStyle
+				} else {
+					style = t.incorrectStyle
+				}
+			} else {
+				style = t.correctStyle
+			}
 
-            scr.SetContent(cx, cy, text[i], nil, style)
-            cx++
-        }
+			scr.SetContent(cx, cy, text[i], nil, style)
+			cx++
+		}
 
-        aw, ah := calcStringDimensions(attribution)
-        drawString(t.Scr, x+nc-aw, y+nr+1, attribution, -1, t.defaultStyle)
+		aw, ah := calcStringDimensions(attribution)
+		drawString(t.Scr, x+nc-aw, y+nr+1, attribution, -1, t.defaultStyle)
 
-        if timeLimit != -1 && !startTime.IsZero() {
-            remaining := timeLimit - time.Now().Sub(startTime)
-            drawString(t.Scr, x+nc/2, y+nr+ah+1, "      ", -1, t.defaultStyle)
-            drawString(t.Scr, x+nc/2, y+nr+ah+1, strconv.Itoa(int(remaining/1e9)+1), -1, t.defaultStyle)
-        }
+		if timeLimit != -1 && !startTime.IsZero() {
+			remaining := timeLimit - time.Now().Sub(startTime)
+			drawString(t.Scr, x+nc/2, y+nr+ah+1, "      ", -1, t.defaultStyle)
+			drawString(t.Scr, x+nc/2, y+nr+ah+1, strconv.Itoa(int(remaining/1e9)+1), -1, t.defaultStyle)
+		}
 
-        if t.ShowWpm && !startTime.IsZero() {
-            calcStats()
-            if duration > 1e7 { //Avoid flashing large numbers on test start.
-                wpm := int((float64(ncorrect) / 5) / (float64(duration) / 60e9))
-                drawString(t.Scr, x+nc/2-4, y-2, fmt.Sprintf("WPM: %-10d\n", wpm), -1, t.defaultStyle)
-            }
-        }
+		if t.ShowWpm && !startTime.IsZero() {
+			calcStats()
+			if duration > 1e7 { //Avoid flashing large numbers on test start.
+				wpm := int((float64(ncorrect) / 5) / (float64(duration) / 60e9))
+				drawString(t.Scr, x+nc/2-4, y-2, fmt.Sprintf("WPM: %-10d\n", wpm), -1, t.defaultStyle)
+			}
+		}
 
-        //Potentially inefficient, but seems to be good enough
+		//Potentially inefficient, but seems to be good enough
 
-        t.Scr.Show()
-    }
+		t.Scr.Show()
+	}
 
-    deleteWord := func() {
-        if idx == 0 {
-            return
-        }
+	deleteWord := func() {
+		if idx == 0 {
+			return
+		}
 
-        idx--
+		idx--
 
-        for idx > 0 && (text[idx] == ' ' || text[idx] == '\n') {
-            idx--
-        }
+		for idx > 0 && (text[idx] == ' ' || text[idx] == '\n') {
+			idx--
+		}
 
-        for idx > 0 && text[idx] != ' ' && text[idx] != '\n' {
-            idx--
-        }
+		for idx > 0 && text[idx] != ' ' && text[idx] != '\n' {
+			idx--
+		}
 
-        if text[idx] == ' ' || text[idx] == '\n' {
-            typed[idx] = text[idx]
-            idx++
-        }
-    }
+		if text[idx] == ' ' || text[idx] == '\n' {
+			typed[idx] = text[idx]
+			idx++
+		}
+	}
 
-    tickerCloser := make(chan bool)
+	tickerCloser := make(chan bool)
 
-    //Inject nil events into the main event loop at regular invervals to force an update
-    ticker := func() {
-        for {
-            select {
-            case <-tickerCloser:
-                return
-            default:
-            }
+	//Inject nil events into the main event loop at regular invervals to force an update
+	ticker := func() {
+		for {
+			select {
+			case <-tickerCloser:
+				return
+			default:
+			}
 
-            time.Sleep(time.Duration(5e8))
-            t.Scr.PostEventWait(nil)
-        }
-    }
+			time.Sleep(time.Duration(5e8))
+			t.Scr.PostEventWait(nil)
+		}
+	}
 
-    go ticker()
-    defer close(tickerCloser)
+	go ticker()
+	defer close(tickerCloser)
 
-    if startImmediately {
-        startTime = time.Now()
-    }
+	if startImmediately {
+		startTime = time.Now()
+	}
 
-    t.Scr.Clear()
-    for {
-        redraw()
+	t.Scr.Clear()
+	for {
+		redraw()
 
-        ev := t.Scr.PollEvent()
+		ev := t.Scr.PollEvent()
 
-        switch ev := ev.(type) {
-        case *tcell.EventResize:
-            rc = TyperResize
-            return
-        case *tcell.EventKey:
-            if runtime.GOOS != "windows" && ev.Key() == tcell.KeyBackspace { //Control+backspace on unix terms
-                if !t.DisableBackspace {
-                    deleteWord()
-                }
-                continue
-            }
+		switch ev := ev.(type) {
+		case *tcell.EventResize:
+			rc = TyperResize
+			return
+		case *tcell.EventKey:
+			if runtime.GOOS != "windows" && ev.Key() == tcell.KeyBackspace { //Control+backspace on unix terms
+				if !t.DisableBackspace {
+					deleteWord()
+				}
+				continue
+			}
 
-            if startTime.IsZero() {
-                startTime = time.Now()
-            }
+			if startTime.IsZero() {
+				startTime = time.Now()
+			}
 
-            switch key := ev.Key(); key {
-            case tcell.KeyCtrlC:
-                rc = TyperSigInt
+			switch key := ev.Key(); key {
+			case tcell.KeyCtrlC:
+				rc = TyperSigInt
 
-                return
-            case tcell.KeyEscape:
-                rc = TyperEscape
+				return
+			case tcell.KeyEscape:
+				rc = TyperEscape
 
-                return
-            case tcell.KeyCtrlL:
-                t.Scr.Sync()
+				return
+			case tcell.KeyCtrlL:
+				t.Scr.Sync()
 
-            case tcell.KeyRight:
-                rc = TyperNext
-                return
+			case tcell.KeyRight:
+				rc = TyperNext
+				return
 
-            case tcell.KeyLeft:
-                rc = TyperPrevious
-                return
+			case tcell.KeyLeft:
+				rc = TyperPrevious
+				return
 
-            case tcell.KeyCtrlW:
-                if !t.DisableBackspace {
-                    deleteWord()
-                }
+			case tcell.KeyCtrlW:
+				if !t.DisableBackspace {
+					deleteWord()
+				}
 
-            case tcell.KeyBackspace, tcell.KeyBackspace2:
-                if !t.DisableBackspace {
-                    if ev.Modifiers() == tcell.ModAlt || ev.Modifiers() == tcell.ModCtrl {
-                        deleteWord()
-                    } else {
-                        if idx == 0 {
-                            break
-                        }
+			case tcell.KeyBackspace, tcell.KeyBackspace2:
+				if !t.DisableBackspace {
+					if ev.Modifiers() == tcell.ModAlt || ev.Modifiers() == tcell.ModCtrl {
+						deleteWord()
+					} else {
+						if idx == 0 {
+							break
+						}
 
-                        idx--
+						idx--
 
-                        for idx > 0 && text[idx] == '\n' {
-                            idx--
-                        }
-                    }
-                }
-            case tcell.KeyRune:
-                if idx < len(text) {
-                    if t.SkipWord && ev.Rune() == ' ' {
-                        if idx > 0 && text[idx-1] == ' ' && text[idx] != ' ' { //Do nothing on word boundaries.
-                            break
-                        }
+						for idx > 0 && text[idx] == '\n' {
+							idx--
+						}
+					}
+				}
+			case tcell.KeyRune:
+				if idx < len(text) {
+					if t.SkipWord && ev.Rune() == ' ' {
+						if idx > 0 && text[idx-1] == ' ' && text[idx] != ' ' { //Do nothing on word boundaries.
+							break
+						}
 
-                        for idx < len(text) && text[idx] != ' ' && text[idx] != '\n' {
-                            typed[idx] = 0
-                            idx++
-                        }
+						for idx < len(text) && text[idx] != ' ' && text[idx] != '\n' {
+							typed[idx] = 0
+							idx++
+						}
 
-                        if idx < len(text) {
-                            typed[idx] = text[idx]
-                            idx++
-                        }
-                    } else {
-                        typed[idx] = ev.Rune()
-                        idx++
-                    }
+						if idx < len(text) {
+							typed[idx] = text[idx]
+							idx++
+						}
+					} else {
+						typed[idx] = ev.Rune()
+						idx++
+					}
 
-                    for idx < len(text) && text[idx] == '\n' {
-                        typed[idx] = text[idx]
-                        idx++
-                    }
-                }
+					for idx < len(text) && text[idx] == '\n' {
+						typed[idx] = text[idx]
+						idx++
+					}
+				}
 
-                if idx == len(text) {
-                    calcStats()
-                    return
-                }
-            }
-        default: //tick
-            if timeLimit != -1 && !startTime.IsZero() && timeLimit <= time.Now().Sub(startTime) {
-                calcStats()
-                return
-            }
+				if idx == len(text) {
+					calcStats()
+					return
+				}
+			}
+		default: //tick
+			if timeLimit != -1 && !startTime.IsZero() && timeLimit <= time.Now().Sub(startTime) {
+				calcStats()
+				return
+			}
 
-            redraw()
-        }
-    }
+			redraw()
+		}
+	}
 }
--- src/util.go
+++ src/util.go
@@ -1,225 +1,225 @@
 package main
 
 import (
-    "fmt"
-    "io"
-    "math/rand"
-    "os"
-    "path/filepath"
-    "regexp"
-    "strings"
-    "time"
+	"fmt"
+	"io"
+	"math/rand"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strings"
+	"time"
 
-    "github.com/gdamore/tcell"
+	"github.com/gdamore/tcell"
 )
 
 var CONFIG_DIRS []string
 
 func init() {
-    home, _ := os.LookupEnv("HOME")
+	home, _ := os.LookupEnv("HOME")
 
-    CONFIG_DIRS = []string{
-        filepath.Join(home, ".tt"),
-        "/etc/tt",
-    }
+	CONFIG_DIRS = []string{
+		filepath.Join(home, ".tt"),
+		"/etc/tt",
+	}
 }
 
 type cell struct {
-    c     rune
-    style tcell.Style
+	c     rune
+	style tcell.Style
 }
 
 func dbgPrintf(scr tcell.Screen, format string, args ...interface{}) {
-    for i := 0; i < 80; i++ {
-        for j := 0; j < 80; j++ {
-            scr.SetContent(i, j, ' ', nil, tcell.StyleDefault)
-        }
-    }
-    drawString(scr, 0, 0, fmt.Sprintf(format, args...), -1, tcell.StyleDefault)
+	for i := 0; i < 80; i++ {
+		for j := 0; j < 80; j++ {
+			scr.SetContent(i, j, ' ', nil, tcell.StyleDefault)
+		}
+	}
+	drawString(scr, 0, 0, fmt.Sprintf(format, args...), -1, tcell.StyleDefault)
 }
 
 func getParagraphs(s string) []string {
-    s = strings.Replace(s, "\r", "", -1)
-    s = regexp.MustCompile("\n\n+").ReplaceAllString(s, "\n\n")
-    return strings.Split(strings.Trim(s, "\n"), "\n\n")
+	s = strings.Replace(s, "\r", "", -1)
+	s = regexp.MustCompile("\n\n+").ReplaceAllString(s, "\n\n")
+	return strings.Split(strings.Trim(s, "\n"), "\n\n")
 }
 
 func wordWrapBytes(s []byte, n int) {
-    sp := 0
-    sz := 0
+	sp := 0
+	sz := 0
 
-    for i := 0; i < len(s); i++ {
-        sz++
+	for i := 0; i < len(s); i++ {
+		sz++
 
-        if s[i] == '\n' {
-            s[i] = ' '
-        }
+		if s[i] == '\n' {
+			s[i] = ' '
+		}
 
-        if s[i] == ' ' {
-            sp = i
-        }
+		if s[i] == ' ' {
+			sp = i
+		}
 
-        if sz > n {
-            if sp != 0 {
-                s[sp] = '\n'
-            }
+		if sz > n {
+			if sp != 0 {
+				s[sp] = '\n'
+			}
 
-            sz = i - sp
-        }
-    }
+			sz = i - sp
+		}
+	}
 
 }
 
 func wordWrap(s string, n int) string {
-    r := []byte(s)
-    wordWrapBytes(r, n)
-    return string(r)
+	r := []byte(s)
+	wordWrapBytes(r, n)
+	return string(r)
 }
 
 func init() {
-    rand.Seed(time.Now().Unix())
+	rand.Seed(time.Now().Unix())
 }
 
 func randomText(n int, words []string) string {
-    r := ""
+	r := ""
 
-    var last string
-    for i := 0; i < n; i++ {
-        w := words[rand.Int()%len(words)]
-        for last == w {
-            w = words[rand.Int()%len(words)]
-        }
+	var last string
+	for i := 0; i < n; i++ {
+		w := words[rand.Int()%len(words)]
+		for last == w {
+			w = words[rand.Int()%len(words)]
+		}
 
-        r += w
-        if i != n-1 {
-            r += " "
-        }
+		r += w
+		if i != n-1 {
+			r += " "
+		}
 
-        last = w
-    }
+		last = w
+	}
 
-    return strings.Replace(r, "\n", " \n", -1)
+	return strings.Replace(r, "\n", " \n", -1)
 }
 
 func stringToCells(s string) []cell {
-    a := make([]cell, len(s))
-    s = strings.TrimRight(s, "\n ")
+	a := make([]cell, len(s))
+	s = strings.TrimRight(s, "\n ")
 
-    len := 0
-    for _, r := range s {
-        a[len].c = r
-        a[len].style = tcell.StyleDefault
-        len++
-    }
+	len := 0
+	for _, r := range s {
+		a[len].c = r
+		a[len].style = tcell.StyleDefault
+		len++
+	}
 
-    return a[:len]
+	return a[:len]
 }
 
 func drawString(scr tcell.Screen, x, y int, s string, cursorIdx int, style tcell.Style) {
-    sx := x
+	sx := x
 
-    for i, c := range s {
-        if c == '\n' {
-            y++
-            x = sx
-        } else {
-            scr.SetContent(x, y, c, nil, style)
-            if i == cursorIdx {
-                scr.ShowCursor(x, y)
-            }
+	for i, c := range s {
+		if c == '\n' {
+			y++
+			x = sx
+		} else {
+			scr.SetContent(x, y, c, nil, style)
+			if i == cursorIdx {
+				scr.ShowCursor(x, y)
+			}
 
-            x++
-        }
-    }
+			x++
+		}
+	}
 
-    if cursorIdx == len(s) {
-        scr.ShowCursor(x, y)
-    }
+	if cursorIdx == len(s) {
+		scr.ShowCursor(x, y)
+	}
 }
 
 func drawStringAtCenter(scr tcell.Screen, s string, style tcell.Style) {
-    nc, nr := calcStringDimensions(s)
-    sw, sh := scr.Size()
+	nc, nr := calcStringDimensions(s)
+	sw, sh := scr.Size()
 
-    x := (sw - nc) / 2
-    y := (sh - nr) / 2
+	x := (sw - nc) / 2
+	y := (sh - nr) / 2
 
-    drawString(scr, x, y, s, -1, style)
+	drawString(scr, x, y, s, -1, style)
 }
 
 func calcStringDimensions(s string) (nc, nr int) {
-    if s == "" {
-        return 0, 0
-    }
+	if s == "" {
+		return 0, 0
+	}
 
-    c := 0
+	c := 0
 
-    for _, x := range s {
-        if x == '\n' {
-            nr++
-            if c > nc {
-                nc = c
-            }
-            c = 0
-        } else {
-            c++
-        }
-    }
+	for _, x := range s {
+		if x == '\n' {
+			nr++
+			if c > nc {
+				nc = c
+			}
+			c = 0
+		} else {
+			c++
+		}
+	}
 
-    nr++
-    if c > nc {
-        nc = c
-    }
+	nr++
+	if c > nc {
+		nc = c
+	}
 
-    return
+	return
 }
 
 func newTcellColor(s string) (tcell.Color, error) {
-    if len(s) != 7 || s[0] != '#' {
-        return 0, fmt.Errorf("%s is not a valid hex color", s)
-    }
+	if len(s) != 7 || s[0] != '#' {
+		return 0, fmt.Errorf("%s is not a valid hex color", s)
+	}
 
-    tonum := func(c byte) int32 {
-        if c > '9' {
-            if c >= 'a' {
-                return (int32)(c - 'a' + 10)
-            } else {
-                return (int32)(c - 'A' + 10)
-            }
-        } else {
-            return (int32)(c - '0')
-        }
-    }
+	tonum := func(c byte) int32 {
+		if c > '9' {
+			if c >= 'a' {
+				return (int32)(c - 'a' + 10)
+			} else {
+				return (int32)(c - 'A' + 10)
+			}
+		} else {
+			return (int32)(c - '0')
+		}
+	}
 
-    r := tonum(s[1])<<4 | tonum(s[2])
-    g := tonum(s[3])<<4 | tonum(s[4])
-    b := tonum(s[5])<<4 | tonum(s[6])
+	r := tonum(s[1])<<4 | tonum(s[2])
+	g := tonum(s[3])<<4 | tonum(s[4])
+	b := tonum(s[5])<<4 | tonum(s[6])
 
-    return tcell.NewRGBColor(r, g, b), nil
+	return tcell.NewRGBColor(r, g, b), nil
 }
 
 func readResource(typ, name string) []byte {
-    // fmt.Println("readResource", typ, name)
-    if name == "-" {
-        b, err := io.ReadAll(os.Stdin)
-        if err != nil {
-            panic(err)
-        }
+	// fmt.Println("readResource", typ, name)
+	if name == "-" {
+		b, err := io.ReadAll(os.Stdin)
+		if err != nil {
+			panic(err)
+		}
 
-        return b
-    }
+		return b
+	}
 
-    if b, err := os.ReadFile(filepath.Join(typ, name)); err != nil {
-        fmt.Println(err)
-    } else {
-        return b
-    }
+	if b, err := os.ReadFile(filepath.Join(typ, name)); err != nil {
+		fmt.Println(err)
+	} else {
+		return b
+	}
 
-    for _, d := range CONFIG_DIRS {
-        if b, err := os.ReadFile(filepath.Join(d, typ, name)); err == nil {
-            return b
-        }
-    }
+	for _, d := range CONFIG_DIRS {
+		if b, err := os.ReadFile(filepath.Join(d, typ, name)); err == nil {
+			return b
+		}
+	}
 
-    return readPackedFile(filepath.Join(typ, name))
+	return readPackedFile(filepath.Join(typ, name))
 }
